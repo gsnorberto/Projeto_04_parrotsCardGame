29dfe2079c8e08c41f14af9b686acb9b5d579c55
gsnorberto <sn_gabriel@outlook.com>
2022-10-19 10:28:57 -0300

    feat: relógio adicionado

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 let numberOfCards = 0;
 
-while(true){
+//timer
+let m = 0;
+let s = 0;
+let timeout;
+
+while (true) {
     let qnt = prompt('Com quantas cartas quer jogar?');
 
-    if(qnt < 4 || qnt > 14 || qnt % 2 !== 0){
+    if (qnt < 4 || qnt > 14 || qnt % 2 !== 0) {
         alert('Insira um valor válido');
-    } else{
+    } else {
         numberOfCards = qnt;
         break;
     }
 }
+startTimeCount();
 
 let cards_div = document.querySelector('.cards');
 
@@ -23,40 +29,43 @@ let count_rightCards = 0; // 2, 4, 6 ...
 let numberOfMoves = 0; // 1,2,3,4...
 
 // Insert cards to array
-for(let i = 1; i <= (numberOfCards / 2); i++){
-    for(let j = 1; j <= 2 ; j++) {
+for (let i = 1; i <= (numberOfCards / 2); i++) {
+    for (let j = 1; j <= 2; j++) {
         cardsPosition.push(i);
     }
 }
 
+
+
 // Shuffle cards
-cardsPosition.sort(comparador); 
-function comparador() { 
-	return Math.random() - 0.5; 
+cardsPosition.sort(comparador);
+function comparador() {
+    return Math.random() - 0.5;
 }
 console.log(cardsPosition);
 
 // Add cards on display
-for(let i = 0; i < numberOfCards; i++) {
-    cards_div.innerHTML += `<div onclick='turnCard(${i+1})' class='card'> <img src='./media/imgs/back.png' alt=''> </div>`;
+for (let i = 0; i < numberOfCards; i++) {
+    cards_div.innerHTML += `<div onclick='turnCard(${i + 1})' class='card'> <img src='./media/imgs/back.png' alt=''> </div>`;
 }
 
 //Turn Card
 const turnCard = (divPosition) => {
     // if there are no upturned cards or if the clicked cards are different
-    if(numberOfCardsTurned === 0 || upturnedCard_position[0] !== divPosition) {
+    if (numberOfCardsTurned === 0 || upturnedCard_position[0] !== divPosition) {
         numberOfCardsTurned++;
         upturnedCard_position.push(divPosition);
         numberOfMoves++;
-    } 
-    
-    document.querySelector(`.cards .card:nth-child(${divPosition})`).innerHTML = `<img src='./media/imgs/${cardsName[[cardsPosition[divPosition-1]]-1]}' alt=''>`;
+    }
 
-    if(numberOfCardsTurned === 2){12
+    document.querySelector(`.cards .card:nth-child(${divPosition})`).innerHTML = `<img src='./media/imgs/${cardsName[[cardsPosition[divPosition - 1]] - 1]}' alt=''>`;
+
+    if (numberOfCardsTurned === 2) {
+        12
         //disable all click events
         document.querySelectorAll(".card").forEach((element) => element.classList.add("disabled"));
 
-        if(cardsPosition[upturnedCard_position[0]-1] === cardsPosition[upturnedCard_position[1]-1]){ //the cards are the same
+        if (cardsPosition[upturnedCard_position[0] - 1] === cardsPosition[upturnedCard_position[1] - 1]) { //the cards are the same
             //disable click
             document.querySelector(`.cards .card:nth-child(${upturnedCard_position[0]})`).removeAttribute("onclick");
             document.querySelector(`.cards .card:nth-child(${upturnedCard_position[1]})`).removeAttribute("onclick");
@@ -64,14 +73,14 @@ const turnCard = (divPosition) => {
             count_rightCards += 2;
             upturnedCard_position = [];
         } else { //the cards are different
-            setTimeout(function(){
+            setTimeout(function () {
                 //hide card
                 document.querySelector(`.cards .card:nth-child(${upturnedCard_position[0]})`).innerHTML = `<img src='./media/imgs/back.png' alt=''>`;
                 document.querySelector(`.cards .card:nth-child(${upturnedCard_position[1]})`).innerHTML = `<img src='./media/imgs/back.png' alt=''>`;
 
                 upturnedCard_position = [];
             }, 1000);
-            
+
         }
 
         setTimeout(function () {
@@ -82,13 +91,39 @@ const turnCard = (divPosition) => {
     }
 
     console.log(count_rightCards);
-        console.log(numberOfCards);
+    console.log(numberOfCards);
 
-    if(count_rightCards == numberOfCards){
-        
-        setTimeout(function(){
-            alert(`Você ganhou em ${numberOfMoves} jogadas!`);
+    if (count_rightCards == numberOfCards) {
+        stopTime();
+
+        setTimeout(function () {
+            alert(`Você ganhou em ${numberOfMoves} jogadas! Tempo do jogo: ${timeFormat(m)}:${timeFormat(s-1)}`);
+            
         }, 500);
     }
 }
 
+//Time
+function startTimeCount () {
+    document.querySelector('.clock').innerHTML = timeFormat(m) + ":" + timeFormat(s);
+
+    s++;
+    if(s == 60){
+        s = 0;
+        m++;
+    }
+
+    timeout = setTimeout(startTimeCount, 1000);
+}
+
+// stop time
+function stopTime(){
+    clearTimeout(timeout);
+}
+
+function timeFormat (num) {
+    if(num < 10){
+        return '0' + num;
+    }
+    return num;
+}
